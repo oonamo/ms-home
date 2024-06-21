32bf4789732686d9568bef7b0f5e2cc251473055
oonamo <onam75hernandez@gmail.com>
2024-06-21 14:35:17 -0700

    ci: create initial workflows

---
+++ src/install.bat
@@ -0,0 +1,25 @@
+@echo off
+
+rem define installation directory
+set INSTALL_DIR=%APPDATA%\ms_home
+
+if not exist "%INSTALL_DIR%\" (
+    echo making folder at "%INSTALL_DIR%"
+    mkdir "%INSTALL_DIR%"
+)
+
+rem TODO: Package executable in release
+make build_local
+
+rem Copy executable
+echo Installing at "%INSTALL_DIR%\"
+copy /Y "%~dp0ms_home.exe" "%INSTALL_DIR%\"
+copy /Y "%~dp0/include/bin/lua54.dll" "%INSTALL_DIR\"
+
+rem Copy path to a file
+echo %PATH% > "%INSTALL_DIR%\path.bak"
+
+set "INSTALL_DIR=%PATH%;%INSTALL_DIR%"
+reg add HKEY_CURRENT_USER\Environment /v Path /t REG_EXPAND_SZ /d "%INSTALL_DIR%" /f
+
+echo Installed creal.
