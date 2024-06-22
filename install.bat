3eedd5ddd59c4a07cc2af165cd7784682a48b93d
oonamo <onam75hernandez@gmail.com>
2024-06-21 22:03:56 -0700

    fix(install): fix type in lua library install path

--- install.bat
+++ install.bat
@@ -11,12 +11,15 @@ if not exist "%INSTALL_DIR%\" (
 rem TODO: Package executable in release
 make build_local
 
-rem Copy executable
 echo Installing at "%INSTALL_DIR%\"
+
+rem Copy executable
 echo "copying %~dp0ms_home.exe"
 copy /Y "%~dp0build\ms_home.exe" "%INSTALL_DIR%\"
+
+rem Copy dll
 echo "copying %~dp0include\bin\lua54.dll"
-copy /Y "%~dp0include\bin\lua54.dll" "%INSTALL_DIR"
+copy /Y "%~dp0include\bin\lua54.dll" "%INSTALL_DIR\"
 
 rem Copy path to a file
 echo %PATH% > "%INSTALL_DIR%\path.bak"
@@ -24,4 +27,4 @@ echo %PATH% > "%INSTALL_DIR%\path.bak"
 set "INSTALL_DIR=%PATH%;%INSTALL_DIR%"
 reg add HKEY_CURRENT_USER\Environment /v Path /t REG_EXPAND_SZ /d "%INSTALL_DIR%" /f
 
-echo Installed creal.
+echo Installed ms-home.
